582b421957774695c84720930aea4f994fb09f17
Ricardo Valfreixo <ricardo.valfreixo@check24.de>
2020-02-07 17:00:46 +0100

    clear last bugs

--- src/ui/utils/converters.ts
+++ src/ui/utils/converters.ts
@@ -1,43 +1,44 @@
-import { Money } from '@waves/data-entities';
-import { BigNumber } from '@turtlenetwork/bignumber';
+import { Money } from "@waves/data-entities";
+import { BigNumber } from "@waves/bignumber";
 
 export const moneyLikeToMoney = (amount: IMoneyLike, assets): Money => {
-    if (amount) {
-        let amountResult = new Money(0, assets[amount.assetId || 'TN']);
+  if (amount) {
+    let amountResult = new Money(0, assets[amount.assetId || "TN"]);
 
-        if ('tokens' in amount) {
-            amountResult = amountResult.cloneWithTokens(amount.tokens as number || 0);
-        }
-
-        if ('coins' in amount) {
-            amountResult = amountResult.add(amountResult.cloneWithCoins(amount.coins as number || 0));
-        }
-
-        return amountResult;
+    if ("tokens" in amount) {
+      amountResult = amountResult.cloneWithTokens(
+        (amount.tokens as number) || 0
+      );
     }
+
+    if ("coins" in amount) {
+      amountResult = amountResult.add(
+        amountResult.cloneWithCoins((amount.coins as number) || 0)
+      );
+    }
+
+    return amountResult;
+  }
 };
 
-export const getMoney = (amount: TAmount, assets) => {
-    if (amount instanceof Money) {
-        return amount;
-    }
+export const getMoney = (amount: any, assets) => {
+  if (amount instanceof Money) {
+    return amount;
+  }
 
-    if (amount instanceof BigNumber) {
-        return new Money(amount, assets['TN']);
-    }
+  if (amount instanceof BigNumber) {
+    return new Money(amount, assets["TN"]);
+  }
 
-    if (typeof amount === 'object' && (amount.tokens || amount.coins) ) {
-        return moneyLikeToMoney(amount as IMoneyLike, assets);
-    }
+  if (typeof amount === "object" && (amount.tokens || amount.coins)) {
+    return moneyLikeToMoney(amount as IMoneyLike, assets);
+  }
 
-    return new Money(new BigNumber(amount), assets['TN']);
+  return new Money(new BigNumber(amount), assets["TN"]);
 };
-
-type TAmount = IMoneyLike|BigNumber|Money|string|number;
 
 interface IMoneyLike {
-    coins?: string|number|BigNumber;
-    tokens?: string|number|BigNumber;
-    assetId: string;
+  coins?: number | string | BigNumber;
+  tokens?: number | string | BigNumber;
+  assetId: string;
 }
-
